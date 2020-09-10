f3eef20968f5d181a3573e856173fc7119d64c75
Felipe Leal <lealfelipealves@gmail.com>
2020-09-10 14:13:45 -0300

    Validando a data - Part 1

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
@@ -9,10 +10,15 @@ const appointments = [];
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parseDate = startOfHour(parseISO(date));
+  const findAppointmentsInSameDate = appointments.find(appointment =>
+    isEqual(parseDate, appointment.date
+  );
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parseDate,
   };
 
   appointments.push(appointment);
