677e95852d469198793d5baf6bd7d8892bb0a818
Felipe Leal <lealfelipealves@gmail.com>
2020-09-10 16:51:04 -0300

    feat: Validando a data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,21 +4,34 @@ import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 // http://localhost:3333/appointments
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const parseDate = startOfHour(parseISO(date));
+  const parsedDate = startOfHour(parseISO(date));
+
   const findAppointmentsInSameDate = appointments.find(appointment =>
-    isEqual(parseDate, appointment.date
+    isEqual(parsedDate, appointment.date),
   );
+
+  if (findAppointmentsInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
 
   const appointment = {
     id: uuid(),
     provider,
-    date: parseDate,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
